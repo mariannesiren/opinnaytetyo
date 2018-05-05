8add04a25525845b2ef9a2b8a2986b0f83862aa7
Marianne Siren <marianne.siren@gmail.com>
2018-05-05 08:50:35 +0300

    Fixes typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resource :home, only: [:show]
   resources :groups do
-    resources :group_attachments, only: [], path: "attachements" do
-      get "/download", to: "group_attachements#download"
+    resources :group_attachments, only: [], path: "attachments" do
+      get "/download", to: "group_attachments#download"
     end
   end
   root to: "homes#show"
